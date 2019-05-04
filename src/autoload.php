db5377103f47a842a7437892999d3a8092e30227
Rasmus Milesson <rasmus.milesson@tankbar.com>
2019-05-04 18:25:45 +0200

    Fixed autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -2,12 +2,13 @@
 
 spl_autoload_register(
     function ($className) {
-        if (strpos($className, 'Tankbar') === 0) {
-            $className = substr($className, 5, strlen($className));
+        $vendor = 'Tankbar';
+        if (strpos($className, $vendor) === 0) {
+            $className = substr($className, strlen($vendor), strlen($className));
         }
 
         $path = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-        $path = __DIR__ . "/src/$path.php";
+        $path = __DIR__ . "/$path.php";
         if (file_exists($path)) {
             require_once $path;
         }
